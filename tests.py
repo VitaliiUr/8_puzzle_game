045bdc7c5e0916f546422090f67568ac3e5dfb60
VitaliiUr <vitaliyurbanevich@gmail.com>
2021-02-23 16:42:11 +0100

    tests

--- tests.py
+++ tests.py
@@ -4,16 +4,27 @@ from search import Solver
 
 class TestPuzzle(unittest.TestCase):
 
-    def test_ast(self):
-        solver = Solver("ast", [8,6,4,2,1,3,5,7,0])
-        final_state = solver.solve()
-        print(final_state)
-        print(solver.statistics.moves)
-        # self.assertEqual(solver.statistics.moves, ['L', 'U', 'U', 'L', 'D', 'R', 'D', 'L', 'U', 'R', 'R', 'U', 'L', 'L', 'D', 'R', 'R', 'U', 'L', 'D', 'D', 'R', 'U', 'L', 'U', 'L'])
+    puzzle_3_1 = [8,6,4,2,1,3,5,7,0]
+    puzzle_3_2 = [6,1,8,4,0,2,7,3,5]
 
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
+
+    def test_ast1(self):
+        solver = Solver("ast", self.puzzle_3_1)
+        final_state = solver.solve()
+        # print(final_state)
+        # print(solver.statistics.moves)
+        self.assertEqual(solver.statistics.moves, ['L', 'U', 'U', 'L', 'D', 'R', 'D', 'L', 'U', 'R', 'R', 'U', 'L', 'L', 'D', 'R', 'R', 'U', 'L', 'D', 'D', 'R', 'U', 'L', 'U', 'L'])
+    
+    def test_ast2(self):
+        solver = Solver("ast", self.puzzle_3_2)
+        final_state = solver.solve()
+        # print(final_state)
+        # print(solver.statistics.moves)
+        self.assertEqual(solver.statistics.moves, ['D', 'R', 'U', 'U', 'L', 'D', 'R', 'D', 'L', 'U', 'L', 'U', 'R', 'R', 'D', 'D', 'L', 'L', 'U', 'U'])
+
+    # def test_isuper(self):
+    #     self.assertTrue('FOO'.isuper())
+    #     self.assertFalse('Foo'.isuper())
 
     # def test_split(self):
     #     s = 'hello world'
